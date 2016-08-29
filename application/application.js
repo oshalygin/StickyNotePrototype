014265b41041020793583421684b86371108a763
Oleg Shalygin <oshalygin@gmail.com>
2016-08-28 21:05:03 -0700

    general cleanup

---
+++ application/application.js
@@ -0,0 +1,16 @@
+(function () {
+    var noteIdSelector = "#newStickyNote";
+    var stickyNoteSelector = ".PIApostit"
+
+    $(noteIdSelector).click(function (event) {
+        $.PostItAll.new({
+            posX: (event.pageX - 150),
+            posY: (event.pageY + 50)
+        });
+
+        event.preventDefault();
+    });
+
+
+
+})();
--- application/configuration.js
+++ application/configuration.js
@@ -1,81 +1,89 @@
 $(document).ready(function () {
 
     $.fn.postitall.globals = {
-        prefix: '#PIApostit_',//Id note prefixe
-        filter: 'domain',     //Options: domain, page, all
-        savable: false,        //Save postit in storage
-        randomColor: false,         //Random color in new postits
-        toolbar: false,         //Show or hide toolbar
-        autoHideToolBar: false,         //Animation effect on hover over postit showing/hiding toolbar options
-        removable: true,         //Set removable feature on or off
-        askOnDelete: false,         //Confirmation before note removal
-        draggable: true,         //Set draggable feature on or off
-        resizable: false,         //Set resizable feature on or off
-        editable: true,         //Set contenteditable and enable changing note content
-        changeoptions: false,         //Set options feature on or off
-        blocked: false,         //Postit can not be modified
-        minimized: true,         //true = minimized, false = maximixed
-        expand: false,         //Expand note
-        fixed: false,         //Allow to fix the note in page
-        addNew: true,         //Create a new postit
-        showInfo: false,         //Show info icon
-        pasteHtml: false,         //Allow paste html in contenteditor
-        htmlEditor: false,         //Html editor (trumbowyg)
-        autoPosition: true,         //Automatic reposition of the notes when user resizes the screen
-        addArrow: 'back'        //Add an arrow to notes : none, front, back, all
+        prefix: '#PIApostit_',
+        filter: 'domain',
+        savable: false,
+        randomColor: false,
+        toolbar: true,
+        autoHideToolBar: false,
+        removable: true,
+        askOnDelete: false,
+        draggable: true,
+        resizable: false,
+        editable: true,
+        changeoptions: false,
+        blocked: false,
+        minimized: true,
+        expand: false,
+        fixed: false,
+        addNew: true,
+        showInfo: false,
+        pasteHtml: false,
+        htmlEditor: false,
+        autoPosition: true,
+        addArrow: 'back'
     };
 
     $.fn.postitall.defaults = {
-        //Note properties
-        id: "",                       //Note id
-        created: Date.now(),               //Creation date
-        domain: window.location.origin,   //Domain in the url
-        page: window.location.pathname, //Page in the url
-        osname: navigator.appVersion,     //Browser informtion & OS name
-        content: '',                       //Content of the note (text or html)
-        position: 'absolute',               //Position absolute or fixed
-        posX: '10px',                   //x coordinate (from left)
-        posY: '10px',                   //y coordinate (from top)
-        right: '',                       //x coordinate (from right). This property invalidate posX
-        height: 200,                      //Note total height
-        width: 160,                      //Note total width
-        minHeight: 210,                      //Note resizable min-width
-        minWidth: 170,                      //Note resizable min-height
-        oldPosition: {},                       //Position when minimized/collapsed (internal use)
-        //Config note style
+        id: "",
+        created: Date.now(),
+        domain: window.location.origin,
+        page: window.location.pathname,
+        osname: navigator.appVersion,
+        content: '',
+        position: 'absolute',
+        posX: '10px',
+        posY: '10px',
+        right: '',
+        height: 200,
+        width: 160,
+        minHeight: 210,
+        minWidth: 170,
+        oldPosition: {},
         style: {
-            tresd: true,                 //General style in 3d format
-            backgroundcolor: '#ffc',            //Background color in new postits when randomColor = false
-            textcolor: '#333333',            //Text color
-            textshadow: true,                 //Shadow in the text
-            fontfamily: 'verdana',            //Default font
-            fontsize: 'small',              //Default font size
-            arrow: 'none',               //Default arrow : none, top, right, bottom, left
+            tresd: true,
+            backgroundcolor: '#ffc',
+            textcolor: '#333333',
+            textshadow: true,
+            fontfamily: 'verdana',
+            fontsize: 'small',
+            arrow: 'none',
         },
-        //Enable / Disable features
-        features: $.fn.postitall.globals,          //By default, copy of global defaults
-        //Note flags
+        features: $.fn.postitall.globals,
         flags: {
-            blocked: false,                //If true, the note cannot be edited
-            minimized: false,                //true = Collapsed note / false = maximixed
-            expand: false,                //true = Expanded note / false = normal
-            fixed: false,                //Set position fixed
-            highlight: false,                //Higlight note
+            blocked: false,
+            minimized: false,
+            expand: false,
+            fixed: false,
+            highlight: false,
         },
-        //Attach the note to al html element
+
         attachedTo: {
-            element: '',                   //Where to attach
-            position: 'right',              //Position relative to elemente : top, right, bottom or left
-            fixed: true,                 //Fix note to element when resize screen
-            arrow: true,                 //Show an arrow in the inverse position
+            element: '',
+            position: 'right',
+            fixed: true,
+            arrow: true,
         },
-        // Callbacks / Event Handlers
-        onCreated: function (id, options, obj) { return undefined; },    //Triggered after note creation
-        onChange: function (id) { return undefined; },                  //Triggered on each change
-        onSelect: function (id) { return undefined; },                  //Triggered when note is clicked, dragged or resized
-        onDblClick: function (id) { console.log("derp"); },                //Triggered on double click
-        onRelease: function (id) { return undefined; },                 //Triggered on the end of dragging and resizing of a note
-        onDelete: function (id) { return undefined; }                   //Triggered when a note is deleted
+        onCreated: function (id, options, obj) {
+        },
+        onChange: function (id) { return undefined; },
+        onSelect: function (id) { return undefined; },
+
+        onDblClick: function (id) {
+
+            var alreadyFlipped = $(id).hasClass("flip");
+            console.log(alreadyFlipped);
+            if (alreadyFlipped) {
+                $(id).removeClass("flip");
+            } else {
+                $(id).addClass("flip back");
+            }
+
+        },
+
+        onRelease: function (id) { return undefined; },
+        onDelete: function (id) { return undefined; }
     };
 
 
